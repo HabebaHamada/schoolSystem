abbc5b5493b5b333f917f91caf85a0914ceed954
Habeba <habebahamadamohammed@gmail.com>
2025-11-26 10:42:34 +0200

    adding feature that only active users could login

--- app/Http/Middleware/EnsureUserIsActive.php
+++ app/Http/Middleware/EnsureUserIsActive.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class EnsureUserIsActive
 {
@@ -15,6 +16,10 @@ class EnsureUserIsActive
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (Auth::check() && !Auth::user()->active) {
+            Auth::logout();
+            return redirect('/login')->withErrors(['Your account is inactive. Please contact support.']);
+        }
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,15 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified','active'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth','active'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth','active'])->group(function () {
 // Class Routes
 Route::prefix('classes')->group(function () {
     Route::get('/create', [ClassController::class, 'create'])->name('classes.create');
